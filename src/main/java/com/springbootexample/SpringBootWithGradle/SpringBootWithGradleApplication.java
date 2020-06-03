428bf1007e4cd765289176c7bf654f5418fbfabd
midhun.balan@a-cti.com <midhun.balan@anywhere.co>
2020-06-03 07:36:10 +0530

    spring boot with gradle

---
+++ src/main/java/com/springbootexample/SpringBootWithGradle/SpringBootWithGradleApplication.java
@@ -0,0 +1,13 @@
+package com.springbootexample.SpringBootWithGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithGradleApplication.class, args);
+	}
+
+}
